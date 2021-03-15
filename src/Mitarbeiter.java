7a81b41cae7a7d01d130ecddf6131f297a096d7c
HorusXII <79853368+HorusXII@users.noreply.github.com>
2021-03-15 14:34:50 +0100

    Update Mitarbeiter.java

--- src/Mitarbeiter.java
+++ src/Mitarbeiter.java
@@ -1,4 +1,10 @@
 
 public class Mitarbeiter extends Angestellter {
 
+	public Mitarbeiter(String name, String Vorname, String EMail, String Telefon, String Wohnort, String Geburtsjahr,
+			Arbeitsgruppe Arbeitsgruppe) {
+		super(name, Vorname, EMail, Telefon, Wohnort, Geburtsjahr, Arbeitsgruppe);
+		// TODO Auto-generated constructor stub
+	}
+
 }
