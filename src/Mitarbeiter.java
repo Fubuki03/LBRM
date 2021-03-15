9d9833ccafb13d06664e4361dea6a304150f76a7
HorusXII <79853368+HorusXII@users.noreply.github.com>
2021-03-15 15:01:43 +0100

    Update Mitarbeiter.java

--- src/Mitarbeiter.java
+++ src/Mitarbeiter.java
@@ -1,10 +1,52 @@
 
 public class Mitarbeiter extends Angestellter {
 
+	private String Username;
+	private String Password;
+	private int Mitarbnr;
+	private int Vertragsnr;
+
 	public Mitarbeiter(String name, String Vorname, String EMail, String Telefon, String Wohnort, String Geburtsjahr,
-			Arbeitsgruppe Arbeitsgruppe) {
+			Arbeitsgruppe Arbeitsgruppe, String username, String password, int mitarbnr, int vertragsnr) {
 		super(name, Vorname, EMail, Telefon, Wohnort, Geburtsjahr, Arbeitsgruppe);
-		// TODO Auto-generated constructor stub
+		this.Username = username;
+		this.Password = password;
+		this.Mitarbnr = mitarbnr;
+		this.Vertragsnr = vertragsnr;
 	}
+
+	public String getUsername() {
+		return Username;
+	}
+
+	public void setUsername(String username) {
+		Username = username;
+	}
+
+	public String getPassword() {
+		return Password;
+	}
+
+	public void setPassword(String password) {
+		Password = password;
+	}
+
+	public int getMitarbnr() {
+		return Mitarbnr;
+	}
+
+	public void setMitarbnr(int mitarbnr) {
+		Mitarbnr = mitarbnr;
+	}
+
+	public int getVertragsnr() {
+		return Vertragsnr;
+	}
+
+	public void setVertragsnr(int vertragsnr) {
+		Vertragsnr = vertragsnr;
+	}
+	
+	
 
 }
