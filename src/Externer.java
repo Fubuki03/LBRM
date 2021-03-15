92bf8cd84490c92fc6d2e21055d372b0c272cac3
HorusXII <79853368+HorusXII@users.noreply.github.com>
2021-03-15 14:34:43 +0100

    Update Externer.java

--- src/Externer.java
+++ src/Externer.java
@@ -1,4 +1,22 @@
 
 public class Externer extends Angestellter {
+	private int extnr;
+	
+	public Externer(String name, String Vorname, String EMail, String Telefon, String Wohnort, String Geburtsjahr,
+			Arbeitsgruppe Arbeitsgruppe, int extnr) {
+		super(name, Vorname, EMail, Telefon, Wohnort, Geburtsjahr, Arbeitsgruppe);
+		this.extnr = extnr;
+		
+	}
+
+	public int getExtnr() {
+		return extnr;
+	}
+
+	public void setExtnr(int extnr) {
+		this.extnr = extnr;
+	}
+		
+	
 
 }
