ce3c83c3a103a083dd30d60950b9773089dae955
HorusXII <79853368+HorusXII@users.noreply.github.com>
2021-03-22 16:11:38 +0100

    Create InsertPerson.java

---
+++ src/InsertPerson.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class InsertPerson {
+private static ArrayList<Angestellter> userlist = new ArrayList<Angestellter>();
+
+	public static void InsertPerson(Angestellter A ) {
+		userlist.add(A);
+	}
+	
+}
