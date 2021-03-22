e043ae37beaddc7c8e7f30d232a7fc05e72ba3a6
HorusXII <79853368+HorusXII@users.noreply.github.com>
2021-03-22 16:11:34 +0100

    Update Actions.java

--- src/Actions.java
+++ src/Actions.java
@@ -2,20 +2,33 @@ import java.util.ArrayList;
 
 public class Actions {
 
-	public void AddAngestellter (Angestellter A) {
-		if(A.getExternal() == false) {
-			
-			
+	public void AddAngestellter(Angestellter A) {
+		if (A.getExternal() == false) {
+			Angestellter User = new Externer(null, null, null, null, null, null, null, 0, null);
+			User = A;
+			InsertPerson.InsertPerson(User);
+		} else  {
+			Angestellter User = new Mitarbeiter(null, null, null, null, null, null, null, null, null, 0, 0, null);
+			User = A;
+			InsertPerson.InsertPerson(User);
 		}
 	}
-	
-	public ArrayList<Angestellter> getAllPersons () {
+
+	public void EditUser(Angestellter A) {
+
+	}
+
+	public void DeleteUser(Angestellter A) {
+
+	}
+
+	public void FindUser(Angestellter A) {
+
+	}
+
+	public ArrayList<Angestellter> getAll() {
 		ArrayList<Angestellter> userlist = new ArrayList<Angestellter>();
 		return userlist;
 	}
-	
-	public Angestellter get(int ID) {
-		Angestellter A = null;
-		return A;
-	}
+
 }
