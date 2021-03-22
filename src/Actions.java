f83fe2f261cbf3070c0efd1d9583df4c26e97682
HorusXII <79853368+HorusXII@users.noreply.github.com>
2021-03-22 15:49:47 +0100

    Create Actions.java

---
+++ src/Actions.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Actions {
+
+	public void AddAngestellter (Angestellter A) {
+		if(A.getExternal() == false) {
+			
+			
+		}
+	}
+	
+	public ArrayList<Angestellter> getAllPersons () {
+		ArrayList<Angestellter> userlist = new ArrayList<Angestellter>();
+		return userlist;
+	}
+	
+	public Angestellter get(int ID) {
+		Angestellter A = null;
+		return A;
+	}
+}
