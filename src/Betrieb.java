fefb8632cb31d46045c5a3a0a40dfe02bceac3e2
HorusXII <79853368+HorusXII@users.noreply.github.com>
2021-03-22 13:45:38 +0100

    Update Betrieb.java

--- src/Betrieb.java
+++ src/Betrieb.java
@@ -2,23 +2,16 @@
 import java.util.ArrayList;
 
 public class Betrieb {
-	private ArrayList<Angestellter> userlist = new ArrayList<Angestellter>();
 	
+	public Betrieb() {
+		
+	}
 	
-	public ArrayList<Angestellter> getUserlist() {
+	public ArrayList<Angestellter> getAllPersons () {
+		ArrayList<Angestellter> userlist = new ArrayList<Angestellter>();
 		return userlist;
 	}
 	
 	
-	public Angestellter getbyid(int index) {
-		return userlist.get(index);
 		
-	}
-
-
-
-	
-	
-
 }
-
