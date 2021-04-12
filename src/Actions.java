89938dc991f3a4ab4f87a83e649a1302d095b0ec
HorusXII <79853368+HorusXII@users.noreply.github.com>
2021-04-12 16:07:39 +0200

    Update Actions.java

--- src/Actions.java
+++ src/Actions.java
@@ -7,14 +7,13 @@ public class Actions {
 			Angestellter User = new Externer(null, null, null, null, null, null, null, 0, null);
 			User = A;
 			Data.getInstance().InsertPerson(User);
-		} else  {
+		} else {
 			Angestellter User = new Mitarbeiter(null, null, null, null, null, null, null, null, null, 0, 0, null);
 			User = A;
 			Data.getInstance().InsertPerson(User);
 		}
 	}
 
-		
 	public static ArrayList<Arbeitsgruppe> getGroups() {
 		return Data.getInstance().getGroups();
 
@@ -23,14 +22,17 @@ public class Actions {
 	public static ArrayList<Angestellter> getAllUsers() {
 		return Data.getInstance().getAllUsers();
 	}
-	
 
 	public void DeleteUser(Angestellter A) {
 
 	}
 
-	public void FindUser(Angestellter A) {
+	public Angestellter FindUser(Angestellter A) {
+		for (Angestellter element : Data.getInstance().getAllUsers()) {
+			if (element.contains("Java")) {
+				return element;
+			}
+		}
 
 	}
-
 }
