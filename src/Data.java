1c966641afe2421cdc614906aef602eb92416642
HorusXII <79853368+HorusXII@users.noreply.github.com>
2021-04-12 14:13:13 +0200

    Create Data.java

---
+++ src/Data.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Data {
+private static ArrayList<Angestellter> userlist = new ArrayList<Angestellter>();
+private static String[] Groups = {"Poduktions", "Forschung", "Lieferanten"};
+
+	public static void InsertPerson(Angestellter A ) {
+		userlist.add(A);
+	}
+
+	public static String[] getGroups() {
+		return Groups;
+	}
+
+	public void setGroups(String[] groups) {
+		Groups = groups;
+	}
+		
+}
